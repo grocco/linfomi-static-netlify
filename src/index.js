c8f06f5f4c015e0344717a2ecf6bd9e977709f97
Rocco Ghielmini <rocco.ghielmini@rapyuta-robotics.com>
2018-11-19 15:30:20 +0700

    get rid of stripe complitely on server

--- src/index.js
+++ src/index.js
@@ -6,46 +6,7 @@ import { screenResize } from 'domain/state/actions';
 import { window } from 'domain/global';
 import App from './App';
 
-class Main extends React.Component {
-    
-    constructor() {
-        super();
-        this.state = {stripe: null};
-    }
-
-    componentDidMount() {
-      if (window.Stripe) {
-          this.setState({stripe: window.Stripe('pk_test_12345')});
-      } else if (typeof document !== 'undefined') {
-          document.querySelector('#stripe-js').addEventListener('load', () => {
-          // Create Stripe instance once Stripe.js loads
-          this.setState({stripe: window.Stripe('pk_test_12345')});
-          });
-      }
-    
-    }
-
-    render() {
-      if (typeof window !== 'undefined') {
-        const StripeProvider = require('react-stripe-elements').StripeProvider;
-        return (
-            <StripeProvider stripe={this.state.stripe}>
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </StripeProvider>
-        )
-      }
-      
-      return (
-          <Provider store={store}>
-              <App />
-          </Provider>
-        )
-      }
-}
-
-export default Main;
+export default App;
 
 
 // if (typeof window !== 'undefined') {
@@ -74,9 +35,22 @@ export default Main;
 // Render your app
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
   const render = Comp => {
-    renderMethod(
-        <Comp />
-    , document.getElementById('root'))
+    if (typeof window !== 'undefined') {
+      const StripeProvider = require('react-stripe-elements').StripeProvider;
+      renderMethod(
+        <StripeProvider apiKey="pk_test_xpM7sZ6yRB83nlEQu6MAI64U">
+          <Provider store={store}>
+            <Comp />
+          </Provider>
+        </StripeProvider>
+      , document.getElementById('root'))
+    } else {
+      renderMethod(
+          <Provider store={store}>
+            <Comp />
+          </Provider>
+      , document.getElementById('root'))
+    }
   }
 
   // Render!
