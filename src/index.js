613e73949d3b202df5bb83227dd1d358fc47b7c6
Rocco Ghielmini <rocco.ghielmini@rapyuta-robotics.com>
2018-11-19 15:19:44 +0700

    stripe provider loaded only on client..

--- src/index.js
+++ src/index.js
@@ -26,53 +26,70 @@ class Main extends React.Component {
     }
 
     render() {
+      if (typeof window !== 'undefined') {
+        const StripeProvider = require('react-stripe-elements').StripeProvider;
         return (
-            <StripeProvider apiKey="pk_test_xpM7sZ6yRB83nlEQu6MAI64U" key={this.state.stripe}>
+            <StripeProvider stripe={this.state.stripe}>
                 <Provider store={store}>
                     <App />
                 </Provider>
             </StripeProvider>
         )
-    }
+      }
+      
+      return (
+          <Provider store={store}>
+              <App />
+          </Provider>
+        )
+      }
 }
 
 export default Main;
 
 
-if (typeof window !== 'undefined') {
-  const StripeProvider = require('react-stripe-elements').StripeProvider;
+// if (typeof window !== 'undefined') {
+//   const StripeProvider = require('react-stripe-elements').StripeProvider;
 
-  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
-  const render = Comp => {
-    renderMethod(
-      <StripeProvider apiKey="pk_test_xpM7sZ6yRB83nlEQu6MAI64U">
-        <Provider store={store}>
-          <Comp />
-        </Provider>
-      </StripeProvider>
-    , document.getElementById('root'))
-  }
+//   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
+//   const render = Comp => {
+//     renderMethod(
+//       <StripeProvider apiKey="pk_test_xpM7sZ6yRB83nlEQu6MAI64U">
+//         <Provider store={store}>
+//           <Comp />
+//         </Provider>
+//       </StripeProvider>
+//     , document.getElementById('root'))
+//   }
 
-  // Render!
-  render(App)
+//   // Render!
+//   render(App)
 
-  window.addEventListener('resize', () => {
-    store.dispatch(screenResize(window.innerWidth, window.innerHeight));
-  });
-  store.dispatch(screenResize(window.innerWidth, window.innerHeight));
-} else {
+//   window.addEventListener('resize', () => {
+//     store.dispatch(screenResize(window.innerWidth, window.innerHeight));
+//   });
+//   store.dispatch(screenResize(window.innerWidth, window.innerHeight));
+// } else {
 
 // Render your app
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
   const render = Comp => {
     renderMethod(
-        <Provider store={store}>
-          <Comp />
-        </Provider>
+        <Comp />
     , document.getElementById('root'))
   }
 
   // Render!
   render(App)
+
+// }
+
+
+if (typeof window !== 'undefined') {
+
+  window.addEventListener('resize', () => {
+    store.dispatch(screenResize(window.innerWidth, window.innerHeight));
+  });
+  store.dispatch(screenResize(window.innerWidth, window.innerHeight));
 
 }
