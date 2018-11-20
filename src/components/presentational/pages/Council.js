821f3f6ccfc844d2d3cd9daf22008ab9311082b4
Rocco Ghielmini <rocco.ghielmini@rapyuta-robotics.com>
2018-11-20 14:39:21 +0700

    added slug to member

--- src/components/presentational/pages/Council.js
+++ src/components/presentational/pages/Council.js
@@ -9,15 +9,17 @@ const l = (data, field, language) => {
     return data[`${field}-${language}`]
 }
 
-const Member = (member, language) => (<div className="member" key={member.data.slug}>
-<div className="title">{l(member.data, 'title', language)}</div>
-<div className="name-and-surname">{l(member.data, 'name-and-surname', language)}</div>
-<div className="role">{l(member.data, 'role', language)}</div>
-<div className="bio">{l(member.data, 'bio', language)}</div>
-<div className="email">{member.data.email}</div>
-<div className="board-of-directors">{member.data['board-of-directors'] ? { en: 'Board of directors', it: 'Comitato direttivo' }[language] : ''}</div>
-<img className="image" src={member.data.picture} alt="" />
-</div>);
+const Member = (member, language) => (
+    <div className="member" key={member.slug}>
+        <div className="title">{l(member.data, 'title', language)}</div>
+        <div className="name-and-surname">{l(member.data, 'name-and-surname', language)}</div>
+        <div className="role">{l(member.data, 'role', language)}</div>
+        <div className="bio">{l(member.data, 'bio', language)}</div>
+        <div className="email">{member.data.email}</div>
+        <div className="board-of-directors">{member.data['board-of-directors'] ? { en: 'Board of directors', it: 'Comitato direttivo' }[language] : ''}</div>
+        <img className="image" src={member.data.picture} alt="" />
+    </div>
+);
 
 export default withRouteData(({ members, language }) => (
   <div>
